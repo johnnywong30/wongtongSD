1db83e0bf29f885c5a2a7fd01083d8baa9963544
jwongUlti25 <jwong25@stuy.edu>
2018-10-01 18:44:04 -0400

    added 14_login

---
+++ flask_starter_kit/app.py
@@ -0,0 +1,21 @@
+# Team weakKnees
+# Johnny Wong
+# K14 -- Do I Know You?
+# 2018-10-02
+
+from flask import Flask, render_template, request
+app = Flask(__name__) #instantiate flask obj
+
+
+# redirects root dir to /occupations
+@app.route('/')
+def redir():
+
+
+@app.route('/occupations')
+def go():
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
