764d6b066ac97fd8540ea36eeecbf815054695d5
jwongUlti25 <jwong25@stuy.edu>
2018-11-15 14:09:48 -0500

    start k26

---
+++ 26_rrreeesssttt/app.py
@@ -0,0 +1,44 @@
+# Johnny Wong
+# K26 --
+# 2018-11-16
+
+from flask import Flask, render_template, request
+import urllib.request as url_req, urllib.parse as url_parse, json
+app = Flask(__name__) #instantiate flask obj
+
+API_KEY = "34b0833e-2bd7-4614-99d8-cec395f9c68c"
+
+baseurl = "http://version1.api.memegenerator.net//Generators_Select_ByPopular?pageIndex=0&pageSize=6&days=&apiKey={0}".format(API_KEY)
+result = url_req.urlopen(baseurl).read()
+data = json.loads(result)['result']
+print(data)
+'''
+results = data['query']['results']['channel']
+wind_conds = results['wind']
+atmosphere_conds = results['atmosphere']
+astronomy_conds = results['astronomy']
+title = results['item']['title']
+weather = results['item']['condition']
+border = '==================={0}======================'
+
+stuff = (wind_conds, atmosphere_conds, astronomy_conds, title, weather)
+print(border.format('WIND'))
+print(stuff[0])
+print(border.format('ATMOSPHERE'))
+print(stuff[1])
+print(border.format('ASTRONOMY'))
+print(stuff[2])
+print(border.format('TITLE'))
+print(stuff[3])
+print(border.format('WEATHER'))
+print(stuff[4])
+'''
+
+# index
+@app.route('/')
+def index():
+    return render_template('template.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
